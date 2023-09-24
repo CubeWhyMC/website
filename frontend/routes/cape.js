b07164bc1b16b89e5090dadde46517e41e544a9f
CubeWhy <duck_chenmy1903@163.com>
2023-09-24 19:10:40 +0800

    cape

--- frontend/routes/cape.js
+++ frontend/routes/cape.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-    res.render('cape', {});
+router.get('/', function (req, res, next) {
+    res.render('cape', {api: process.env["API-BACKEND"] || "http://127.0.0.1:8080"});
 });
 
 module.exports = router;
