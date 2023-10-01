79667af3c6653865f1c414a2a69e0e86dac748db
CubeWhy <duck_chenmy1903@163.com>
2023-10-01 17:55:04 +0800

    permission

--- backend/src/main/java/org/cubewhy/api/config/SecurityConfiguration.java
+++ backend/src/main/java/org/cubewhy/api/config/SecurityConfiguration.java
@@ -46,6 +46,7 @@ public class SecurityConfiguration {
                                 .requestMatchers("/launcher/**").permitAll()
                                 .requestMatchers("/capes/**").permitAll()
                                 .requestMatchers("/game/**").permitAll()
+                                .requestMatchers("/plugins/**").permitAll()
                                 .requestMatchers("/market/**").permitAll()
                                 .requestMatchers("/api/**").permitAll()
                                 .requestMatchers("/api/customer").rememberMe()
