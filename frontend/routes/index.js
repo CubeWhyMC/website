213984919f82e2fbec9a22e41179eb10d681a5ed
cubewhy <me@lunarclient.top>
2023-12-16 14:10:50 +0800

    crash report page

--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -2,52 +2,71 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {});
+router.get('/', function (req, res, next) {
+    res.render('index', {});
 });
 
-router.get('/farewell', function(req, res, next) {
-  res.render('farewell', {});
+router.get('/farewell', function (req, res, next) {
+    res.render('farewell', {});
 });
 
-router.get('/download/:v', function(req, res, next) {
-  res.redirect("https://github.com/CubeWhyMC/LunarClient-CN/releases/" + req.params.v)
+router.get("/crash", function (req, res) {
+    let crashID = req.query["id"];
+    // TODO request the api server to get info
+    let requestURL = process.env.backend + "launcher/getCrashReport"
+    let trace = "";
+    let type = "";
+    if (crashID === null) {
+        res.render("crash/list", {
+            id: crashID,
+            trace: trace,
+            type: type
+        });
+    } else {
+        res.render("crash/result", {
+
+        })
+    }
+})
+
+router.get('/download/:v', function (req, res, next) {
+    res.redirect("https://github.com/CubeWhyMC/LunarClient-CN/releases/" + req.params.v)
 });
 
-router.get("/thanks", function(req, res) {
-  res.render("thanks"); // process link in the frontend
+router.get("/thanks", function (req, res) {
+    res.render("thanks"); // process link in the frontend
 })
 
 router.get("/download", (req, res) => {
-  res.render('download', {
-    api: process.env.backend
-  });
+    res.render('download', {
+        api: process.env.backend
+    });
 })
 
-router.get('/help', function(req, res, next) {
-  res.redirect("https://github.com/CubeWhyMC/LunarClient-CN/wiki");
+router.get('/help', function (req, res, next) {
+    res.redirect("https://github.com/CubeWhyMC/LunarClient-CN/wiki");
 });
 
-router.get('/en', function(req, res, next) {
-  res.render('index-en', {});
+router.get('/en', function (req, res, next) {
+    res.render('index-en', {});
 });
 
-router.get('/en/download', function(req, res, next) {
-  res.render('download-en', {
-    api: process.env.backend
-  });
+router.get('/en/download', function (req, res, next) {
+    res.render('download-en', {
+        api: process.env.backend
+    });
 });
 
-router.get("/donate", function(req, res) {
-  res.render("donate", {})
+router.get("/donate", function (req, res) {
+    res.render("donate", {})
 });
 
-router.get("/celestial", function(req, res) {
-  res.render("celestial/index")
+router.get("/celestial", function (req, res) {
+    res.render("celestial/index")
 })
 
-router.get("/new", function(req, res) {
-  res.redirect("/celestial")
+router.get("/new", function (req, res) {
+    res.redirect("/celestial")
 })
 
 module.exports = router;
