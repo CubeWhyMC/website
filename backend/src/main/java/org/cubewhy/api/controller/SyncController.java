5115ae39b3d8375a665c9c750b4d3ae9ee20d8d4
cubewhy <me@lunarclient.top>
2024-01-27 09:01:11 +0800

    todo

--- backend/src/main/java/org/cubewhy/api/controller/SyncController.java
+++ backend/src/main/java/org/cubewhy/api/controller/SyncController.java
@@ -1,4 +1,12 @@
 package org.cubewhy.api.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/api/sync")
 public class SyncController {
+    @GetMapping("/missing")
+    public void missing() {
+        // TODO complete api
+    }
 }
